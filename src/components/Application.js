a7296e86270619f6a161692f9b50f70cfa32d944
Suhur Omar <itssuhuromar@gmail.com>
2020-04-18 15:41:57 -0400

    retrieved interview data and build hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,41 +3,48 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
-import { getAppointmentsForDay} from "helpers/selectors";
+import { getAppointmentsForDay, getInterview} from "helpers/selectors";
 
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = day => setState({...state, day});
-  const appointments = getAppointmentsForDay(state, state.day).map(a => {
-    return(
-      <Appointment id={a.id} time={a.time} interview={a.interview} />
-    )
-  })
+
   useEffect(()=>{
     axios.get('http://localhost:8001/api/days')
       .then(response =>{
-        console.log(response.data)
+        // console.log(response.data)
         Promise.all([
           Promise.resolve(axios.get("http://localhost:8001/api/days")),
-          Promise.resolve(axios.get("http://localhost:8001/api/appointments"))
-        ]).then((all) => {
-          console.log(all[0]); // first
-          console.log(all[1]); // second
-          const [first, second] = all;
-          console.log(first.data, second.data);
-
+          Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
+          Promise.resolve(axios.get("http://localhost:8001/api/interviewers")),
+        ]).then(all => {
           setState(prev => ({ 
             ...prev,
             days: all[0].data, 
-            appointments: all[1].data}));
+            appointments: all[1].data,
+            interviewers: all[2].data}));
         });
       })
   }, [])
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -57,7 +64,7 @@ export default function Application(props) {
         lt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
-        {appointments}
+        {schedule}
       </section>
     </main>
   );
