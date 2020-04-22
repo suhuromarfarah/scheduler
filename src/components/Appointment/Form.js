13151603325d27bf0baf9dfc57b94813ca69f671
Suhur Omar <itssuhuromar@gmail.com>
2020-04-22 17:25:07 -0400

    added tests to files and test coverage

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,9 +16,23 @@ export default function Form(props){
     props.onCancel();
   }
 
-  function save(){
-    props.onSave(name, interviewer)
+  // function save(){
+  //   props.onSave(name, interviewer)
+  // }
+  const [error, setError] = useState("");
+  
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  if (interviewer === null) {
+    setError("An interviewer must be chosen");
+    return;
   }
+  setError("");
+  props.onSave(name, interviewer);
+}
 
   return(
     <main className="appointment__card appointment__card--create">
@@ -32,7 +46,9 @@ export default function Form(props){
           value={name}
           onChange={event => { 
             setName(event.target.value)}}
+          data-testid="student-name-input"
         />
+        <section className="appointment__validation">{error}</section>
       </form>
       <InterviewerList 
         interviewers={props.interviewers} 
@@ -43,7 +59,7 @@ export default function Form(props){
     <section className="appointment__card-right">
       <section className="appointment__actions">
         <Button onClick={cancel} danger>Cancel</Button>
-        <Button onClick={save} confirm >Save</Button>
+        <Button onClick={validate()} confirm >Save</Button>
       </section>
     </section>
     </main>
